ff3157f4c07e94207a930866aef048306ff7e036
Kyutae-Chae <ktchae83@gmail.com>
2022-05-16 12:40:34 +0900

    add comments & move scan.close()

--- src/Example07/Example07_v1.java
+++ src/Example07/Example07_v1.java
@@ -9,18 +9,20 @@ public class Example07_v1 {
         System.out.println("[안내]피보나치 수열 프로그램 시작.");
         System.out.print("원하는 수열의 개수를 입력해 주세요 : ");
         inputCnt = scan.nextInt();
+        scan.close();
 
-        int [] result = new int[inputCnt+1];
+        int [] result = new int[inputCnt+1]; //피보나치 수열의 값들을 계산해서 넣을 배열
+        //1, 2번째 초기 값 입력
         if (inputCnt > 0) result[1] = 1;
         if (inputCnt > 1) result[2] = 1;
-        for (int i=3; i<=inputCnt; i++) {
+        for (int i=3; i<=inputCnt; i++) {   // 이후의 값을 아래 점화식으로 채워넣기
             result[i] = result[i-2] + result[i-1];
         }
+
+        //결과 출력
         System.out.println("[피보나치 수열 출력]");
-        for (int i=1; i<=inputCnt; i++) {
+        for (int i=1; i<=inputCnt; i++) {   // 수열을 형식에 맞게 출력
             System.out.println(result[i]);
         }
-
-        scan.close();
     }
 }
