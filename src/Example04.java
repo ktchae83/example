d76bdd49fcab249010e4cdd1ec671b2ba1fee154
Kyutae-Chae <ktchae83@gmail.com>
2022-05-16 14:18:02 +0900

    add comments on Example04

--- src/Example04.java
+++ src/Example04.java
@@ -3,15 +3,15 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Example04 {
-    private static final String YOUTUBE_ID = "admin";
-    private static final String YOUTUBE_PW = "0000";
+    private static final String YOUTUBE_ID = "admin"; //유튜브 계정 아이디
+    private static final String YOUTUBE_PW = "0000"; //유튜브 계정 비밀번호
 
     public static void main(String[] args) {
         String inputID = "";
         String inputPW = "";
         String inputCaption = "";
-        final String[] strNotAllowedWords = {"킹", "시발", "꺼저", "미친"};
-        final String[] strChangedWords = {"열", "사랑", "행운", "희망"};
+        final String[] strNotAllowedWords = {"킹", "시발", "꺼저", "미친"}; //금칙어 리스트
+        final String[] strChangedWords = {"열", "사랑", "행운", "희망"}; //순화어 리스트, 금칙어와 1:1 매핑이 안되는 케이스 처리필요
 //        final HashMap<String, String> hmBeforeAfterWords = new HashMap<String,String>() {{
 //            put("킹", "열");
 //            put("시발", "사랑");
@@ -38,13 +38,13 @@ public class Example04 {
         inputCaption = sc.nextLine();
         System.out.println("========================================");
         System.out.print("[");
-        System.out.print(Arrays.toString(strNotAllowedWords));
+        System.out.print(Arrays.toString(strNotAllowedWords)); //등록된 금칙어 리스트 출력
         System.out.println("]");
         System.out.println("========================================");
 
         System.out.println("[알림] 자막 순화 프로그램 결과입니다.");
         for (int i = 0; i<strNotAllowedWords.length; i++) {
-            inputCaption = inputCaption.replace(strNotAllowedWords[i], strChangedWords[i]);
+            inputCaption = inputCaption.replace(strNotAllowedWords[i], strChangedWords[i]); //금칙어를 순화어로 변경하기
         }
         System.out.printf(">>> %s\n", inputCaption);
         System.out.println("[안내] 프로그램을 종료합니다.");
